3502130217ac0f0b0905395626ba504c10ee7dba
Sunny Li <jinghengsunny@gmail.com>
2024-03-14 15:56:05 -0700

    updated profile page with styling

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,85 +1,83 @@
 "use client"
-
 import { Avatar, Box, Card, Grid, Inset, Text} from "@radix-ui/themes";
 import React, { useEffect, useState } from "react";
-import PersonProfile from "../components/PersonProfile";
 import ReviewCard from "../components/ReviewCard";
 import axios from "axios";
 import {auth} from '../components/firebase';
 import ProgressBar from "../components/ProgressBar";
 
+interface Review {
+    game_id: number;
+    review: string;
+    rating: number;
+  }
+
 const ProfilePage = () => {
-    // Display the user's information, along with their reviews
- 
-    // auth.onAuthStateChanged(auth, (currentUser) => {
-    //     const user = auth.currentUser;
-    //     console.log("User: ", user);
-    //     const user_id = user.uid;
-    // })
-    const user_id = 14;
-    const[profile, setProfile] = useState('');
-    const[reviewList, setReviewsList] = useState([]);
-    const[games, setGames] = useState('');
 
-    const getUserProfile = async () => {
-        const url = 'http://localhost:8080/user/' + user_id;
-        //const url = 'http://localhost:8080/user/13';
-            //useEffect(() => {
+    const [reviewList, setReviewsList] = useState<Review[]>([]);
+    const [userId, setUserId] = useState(null);
+    const [userEmail, setUserEmail] = useState(null);
+    const [username, setUsername] = useState(null);
+    const [reviewedGames, setReviewedGames] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fetchUserId = async () => {
+        const user = auth.currentUser;
+
+        if (user) {
+            const response = await axios.get(`${process.env.BACKEND_URL}/user/email/${user.email}`);
+            setUserId(response.data.id);
+            setUserEmail(response.data.email);
+            setUsername(response.data.username);
+        }
+        };
+
+        fetchUserId();
+    }, []);
+
+    useEffect(() => {
+        const getReviews = async () => {
+        if (userId) {
+            const reviewURL = `${process.env.BACKEND_URL}/review/user/${userId}`;
             try {
-                const profileResponse = await axios({
-                    method: 'GET',
-                    url: url,
-                    //responseType: 'text'
-                });
-                //console.log("Data: ", profileResponse.data);
-                setProfile(profileResponse.data);
-                //return profileResponse.data;
-            }
-            catch(error) {
-                console.log("Error: Failed to get profile.", error.message);
-            }
-    }
+            const reviewResponse = await axios.get(reviewURL);
 
-    const getReviews = async () => {
-        const reviewURL = 'http://localhost:8080/review/user/' + user_id;
-        try {
-        const reviewResponse = await axios({
-            method: 'GET',
-            url: reviewURL,
-            //responseType: 'text'
-        });
-            console.log("reviews: ", reviewResponse.data);
             setReviewsList(reviewResponse.data);
-        }catch(error){
-            console.log("Error: Failed to get reviews.", error.message);
-        }
-    }
 
-    const getGameNameImage = async (game_id) => {
-        const gameURL = 'http://localhost:8080/game/' + game_id;
-        try {
-        const gameResponse = await axios({
-            method: 'GET',
-            url: gameURL,
-            //responseType: 'text'
-        });
-            console.log("Game: ", gameResponse.data);
-            return gameResponse.data;
-        }catch(error){
-            console.log("Error: Failed to get game.", error.message);
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.log("Error: Failed to get reviews.", error.message);
+                } else {
+                    console.log("An error occurred while fetching reviews.");
+                }
+            }
         }
-    }
-    getUserProfile();
-    getReviews();
-    //console.log("Data: ", profile);
+        };
 
-    if(!profile){
-       return <div>Loading profile...</div>
-    }
-    //if(!reviewList){
-    //    return <div>Loading reviews...</div>
-    //}
-    //console.log("data: ", profile);
+        getReviews();
+    }, [userId]);
+
+    useEffect(() => {
+        const getReviewedGames = async () => {
+          if (reviewList.length > 0) {
+            try {
+              const gamesPromises = reviewList.map(review => axios.get(`${process.env.BACKEND_URL}/game/${review.game_id}`));
+              const reviewedGamesNamesPromises = await Promise.all(gamesPromises);
+              const gameNamesData: string[] = reviewedGamesNamesPromises.map(response => response.data.name);
+              setReviewedGames(gameNamesData);
+              console.log(reviewedGames);          
+            } catch (error) {
+              if (error instanceof Error) {
+                  console.log("Error: Failed to get reviews.", error.message);
+                } else {
+                  console.log("An error occurred while fetching reviews.");
+                }   
+            }
+          } 
+        };
+        getReviewedGames();
+      }, [reviewList]);
+    
     return(
         <div style = {{display: 'flex', flexDirection: 'row'}}>
             <div style={{ display: 'grid', gap:'20px'}}>
@@ -89,10 +87,10 @@ const ProfilePage = () => {
                             <div style={{ flex: 1, position: 'relative', paddingLeft: 20, paddingRight: 20, paddingTop: 20, paddingBottom: 20}}> 
                                 <Inset clip="padding-box" side="left">
                                     <Text as="div" size="6" weight="bold">
-                                        {profile.username}
+                                        {username}
                                     </Text>
                                     <Text as="div" size="4" weight="bold">
-                                        {profile.email}
+                                        {userEmail}
                                     </Text>
                                 </Inset>
                             </div>
@@ -102,32 +100,44 @@ const ProfilePage = () => {
             </div>
             <div style={{ display: 'grid', gap:'20px' }}>
             <div>
-                {reviewList.map(review => (  
-                <div>
-                    <Card size="2">
-                        <div style={{ display: 'flex', flexDirection: 'column', gap: 10, alignItems: 'center', justifyContent: 'space-around', height: ' 100%'}}>
-                            <div style={{ flex: 1, position: 'relative', paddingLeft: 20, paddingRight: 20, paddingTop: 20, paddingBottom: 20}}> 
+                {reviewList && reviewList.length > 0 ? (
+                    <div style={{ display: 'grid', gap: '20px' }}>
+                    {reviewList.map((review, index) => (
+                        <div key={index}>
+                        <Card size="2">
+                            <div style={{ display: 'flex', flexDirection: 'column', gap: 10, alignItems: 'center', justifyContent: 'space-around', height: '100%' }}>
+                            <div style={{ flex: 1, position: 'relative', paddingLeft: 20, paddingRight: 20, paddingTop: 20, paddingBottom: 20 }}>
                                 <Box>
+                                <a href={`/reviews/${review.game_id}`}>
                                     <Text as="div" size="2" weight="bold">
-                                        {getGameNameImage(review.game_id).name}
+                                    {reviewedGames[index]}
                                     </Text>
-                                    <Avatar 
-                                        size="6" 
-                                        src = {getGameNameImage(review.game_id).image_link}
-                                        fallback="An image of the game."   
-                                    />
-                                    <Text as="div" size="2" color="gray">
-                                        {review.review} 
-                                    </Text>
-                                    <Text as="div" size="2" color="yellow">
-                                        <ProgressBar initialValue={review.rating} finalValue={100}/> 
-                                    </Text>
+                                </a>
+                                {/* <Avatar 
+                                    size="6" 
+                                    src={getGameNameImage(review.game_id).image_link}
+                                    fallback="An image of the game."   
+                                /> */}
+                                <Text as="div" size="2" color="gray">
+                                    {review.review} 
+                                </Text>
+                                <Text as="div" size="2" color="yellow">
+                                    <ProgressBar initialValue={review.rating} finalValue={100}/> 
+                                </Text>
                                 </Box>
                             </div>
+                            </div>
+                        </Card>
                         </div>
-                    </Card>
+                    ))}
+                    </div>
+                ) : 
+                <div>
+                    <Text as="div" size="2" weight="bold">
+                    No reviews found.
+                    </Text>
                 </div>
-                ))}
+                }
             </div>
       </div>
     </div>
